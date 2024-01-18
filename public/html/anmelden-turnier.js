99f12ab7e0ae3e813d2a7ec90175b25f0f4ffdef
amy-hgn <sandya1911@gmail.com>
2024-01-18 20:35:31 +0100

    user id

--- public/html/anmelden-turnier.js
+++ public/html/anmelden-turnier.js
@@ -63,7 +63,7 @@ async function speichernButtonClick () {
   const urlParams = new URLSearchParams(window.location.search)
   const turnierId = urlParams.get('id')
 
-  const user = await getUserID();
+ // const user = await getUserID();
   // Speicherung turnierid user in object
   const myObjekt = { turnierId, initialize }
   try {
